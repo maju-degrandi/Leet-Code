bf433c4aeb530effb6ae17b5a93b9df0322db26c
Maria Júlia Soares De Grandi <82612544+maju-degrandi@users.noreply.github.com>
2024-05-09 13:53:02 -0300

    Create 383-leetcode(fun-version).cpp

---
+++ 383-leetcode(fun-version).cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        bool ans = true;
+        for(int i = 0; i < 26; i++)
+        {
+            ans = ans && count(ransomNote.begin(), ransomNote.end(), char('a' + i)) 
+                         - count(magazine.begin(), magazine.end(), char('a' + i)) <= 0;
+        }
+        return ans;
+    }
+};
